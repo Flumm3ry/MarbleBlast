026d691300422c242a935e0dc00f16634152a9c5
Jeremy Maxwell <jeremy.v.maxwell@bigpond.com>
2021-07-18 18:03:52 +1000

    Add janky ass powerups

---
+++ Assets/Scripts/Powerup.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Powerup : MonoBehaviour
+{
+    public delegate void OnDestroyedHandler();
+    public event OnDestroyedHandler OnDestroyed;
+    
+    void OnDisable()
+    {
+        if (OnDestroyed != null)
+            OnDestroyed();
+    }
+    void OnCollisionEnter(Collision other)
+    {
+        if (!other.gameObject.CompareTag("Player")) return;
+        
+        other.gameObject.GetComponent<PlayerController>()?.AddPowerup("Some Powerup");
+        
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/PowerupSpawnController.cs
@@ -0,0 +1,57 @@
+using System;
+using UnityEngine;
+
+public class PowerupSpawnController : MonoBehaviour
+{
+    [SerializeReference]
+    private int spawnDelay = 10;
+
+    [SerializeReference]
+    private int variance = 3;
+
+    [SerializeReference]
+    private bool spawnOnStart = false;
+
+    [SerializeReference]
+    private GameObject[] powerups;
+
+    private DateTime nextSpawnTime;
+
+    void Start()
+    {
+        if (spawnOnStart) SpawnPowerup();
+        else SetNextPowerupTime();
+    }
+
+    void Update()
+    {
+        if (!HasPowerup() && DateTime.Now >= nextSpawnTime) SpawnPowerup();
+    }
+
+    void SetNextPowerupTime()
+    {
+        nextSpawnTime = DateTime.Now.AddSeconds(spawnDelay + Offset);
+    }
+
+    void SpawnPowerup()
+    {
+        GameObject powerup = Instantiate(powerups[new System.Random(DateTime.Now.Millisecond).Next(powerups.Length)], transform);
+        powerup.GetComponent<Powerup>().OnDestroyed += SetNextPowerupTime;
+    }
+
+    void OnPowerupDestroyed() {
+        SetNextPowerupTime();
+    }
+
+    bool HasPowerup() {
+        return GetComponentInChildren<Powerup>() != null;
+    }
+
+    int Offset
+    {
+         get
+         {
+             return new System.Random(DateTime.Now.Millisecond).Next(variance * 2) - variance;
+         }
+    }
+}
